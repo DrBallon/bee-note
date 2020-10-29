0f91f9f8b5ef6f77e5f2c2473587a47f4656560a
1144967728@qq.com <1144967728@qq.com>
2020-10-29 21:25:58 +0800

    demo测试

--- src/store/constants/theme.ts
+++
@@ -1,1 +0,0 @@
-export const GET_THEME = 'GET_THEME';
--- src/styles/stylesFunction.tsx
+++
@@ -1,3 +0,0 @@
-// import { BaseTheme } from './theme';
-import { BaseTheme } from '../store/store';
-export const border = (theme: BaseTheme) => `border:1px solid ${theme.borderColor}`;
